f0872cb3d13ed5be0070e5b2bfcce97e90a167a3
Muhammad Hammad <hayw6121@gmail.com>
2025-08-14 02:07:13 +0500

    Add files via upload

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import ProductListingPage from './Product/ProductListingPage';
+const Product = () => {
+  return (
+    <div className="min-h-screen">
+      <ProductListingPage />
+    </div>
+  );
+};
+
+export default Product;
