fa8b721f97810cfac20b12213a363cffded44519
Umair-Rai <umair.akramcs@gmail.com>
2025-08-16 12:03:27 +0500

    Delete Backend/utils directory

--- Backend/utils/validateCategory.js
+++
@@ -1,7 +0,0 @@
-// utils/validateCategory.js
-module.exports = function validateCategory(data) {
-  const errors = [];
-  if (!data.name) errors.push('Name is required');
-  if (!['Cat', 'Dog'].includes(data.pet_type)) errors.push('Pet type must be Cat or Dog');
-  return errors;
-};
